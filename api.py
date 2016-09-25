b7a82c19ce84e8f5aeacb1e043a708769537e28b
AdamConverse <adam.converse@gmail.com>
2016-09-25 05:32:39 -0500

    Live market scrape

--- api.py
+++ api.py
@@ -1,15 +1,20 @@
 import config
-import fut
 import json
 import urllib
 import json
 import pandas as pd
 
 
-def get_all_cards(pages=1):
+def get_all_cards(pages=0):
     """Get all cards from EA."""
     good_columns = ["id", "baseId", "name", "position", "quality", "color", "rating"]
     temp = []
+    url = "https://www.easports.com/fifa/ultimate-team/api/fut/item?page=1"
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+    total_pages = data["totalPages"]
+    if pages is 0:
+        pages = total_pages
     for page in range(pages):
         url = "https://www.easports.com/fifa/ultimate-team/api/fut/item?page=" + str(page)
         response = urllib.urlopen(url)
@@ -26,11 +31,27 @@ def get_all_cards(pages=1):
 
 def post_player(data, cur):
     """Post player to database."""
-    cur.execute("""INSERT INTO fut_players (id, name, club_id, position, quality, card_type, base_id, rating, nation) VALUES
-    (%s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING""", [int(data["id"]), data["name"], int(data["club_id"]), data["position"], data["quality"], data["color"], int(data["baseId"]), int(data["rating"]), data["nation"]])
+    cur.execute("""INSERT INTO fut_players (id, name, club_id, position, quality, card_type, base_id, rating, nation)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        ON CONFLICT DO NOTHING""",
+        [int(data["id"]), data["name"], int(data["club_id"]), data["position"], data["quality"], data["color"], int(data["baseId"]), int(data["rating"]), data["nation"]])
     return
 
 
-def temp():
-    """."""
-    fut = fut.Core(config.email, config.password, config.secret_answer, code=config.code, platform=config.platform, debug=True)
+def post_transaction(data, cur):
+    """Post transaction to database."""
+    cur.execute("""INSERT INTO fut_transactions (sellerEstablished, rating, itemType, resourceId, expires, cardType, formation,
+        leagueId, watched, tradeState, id, sellerId, owners, tradeId, assetId, sellerName, buyNowPrice, injuryType, lastSalePrice, offers, startingBid,
+        timestamp, itemState, currentBid, bidState, rareflag)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        ON CONFLICT DO NOTHING""",
+        [data["sellerEstablished"], data["rating"], data["itemType"], data["resourceId"], data["expires"], data["cardType"], data["formation"], data["leagueId"], data["watched"],
+        data["tradeState"], data["id"], data["sellerId"], data["owners"], data["tradeId"], data["assetId"], data["sellerName"], data["buyNowPrice"], data["injuryType"], data["lastSalePrice"],
+        data["offers"], data["startingBid"], data["timestamp"], data["itemState"], data["currentBid"], data["bidState"], data["rareflag"]])
+    return
+
+
+def get_player(player_id, cur):
+    """Get player from database by id."""
+    cur.execute("""SELECT * FROM fut_players WHERE id = %s;""", [player_id])
+    return cur.fetchone()
--- main.py
+++ main.py
@@ -1,25 +1,52 @@
 import psycopg2
 import api
+import fut
 import config
 import sys
+import time
 
-try:
-    conn = psycopg2.connect(host=config.host, user=config.username, password=config.password, port=config.port, database='fut')
-except:
-    print "connection failed.\nExiting..."
-    sys.exit()
 
-cur = conn.cursor()
-cards = api.get_all_cards(pages=611)
-i = 0
+def update_player_database(cur, pages=0):
+    """Update player FUT database. Skips card if found by id."""
+    cards = api.get_all_cards(pages)
+    i = 0
+    for index, row in cards.iterrows():
+        api.post_player(row, cur)
+        i = i + 1
+        sys.stdout.write("Number of Cards: %d   \r" % (i))
+        sys.stdout.flush()
+    print "\ndone"
+    return
 
-for index, row in cards.iterrows():
-    api.post_player(row, cur)
-    i = i + 1
-    sys.stdout.write("Number of Cards: %d   \r" % (i) )
-    sys.stdout.flush()
-print "\ndone"
 
-conn.commit()
-cur.close()
-conn.close()
+def stream_market_scrape(cur, pages=0):
+    """Stream output of current progress of market scrape."""
+    fut_conn = fut.Core(config.email, config.password, config.secret_answer, code=config.code, platform=config.platform, debug=True)
+    i = 0
+    if pages is 0:
+        pages = 100
+    for page in range(pages):
+        items = fut_conn.searchAuctions('player', start=100+page, level='gold')
+        for item in items:
+            api.post_transaction(item, cur)
+            i = i + 1
+            sys.stdout.write("Number of Cards: %d   \r" % (i))
+            sys.stdout.flush()
+        time.sleep(.1)
+    print "\ndone"
+    return
+
+
+if __name__ == "__main__":
+    try:
+        conn = psycopg2.connect(host=config.host, user=config.username, password=config.password, port=config.port, database='fut')
+    except:
+        print "connection failed.\nExiting..."
+        sys.exit()
+    cur = conn.cursor()
+
+    stream_market_scrape(cur)
+
+    conn.commit()
+    cur.close()
+    conn.close()
